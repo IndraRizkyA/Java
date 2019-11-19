6ad6b71a0878127f58e80f10796d4d490206a0a8
unknown <IndraRizkyA>
2019-11-19 14:25:00 +0700

    JAVA

---
+++ L1.Menampilkan Data Diri/src/l1/menampilkan/data/diri/L1MenampilkanDataDiri.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package l1.menampilkan.data.diri;
+
+/**
+ * NAMA  : Indra Rizky Anfasa
+ * NIM   : A2.1900083
+ * Kelas : TI-1B
+ * 
+ * @author Indra Rizky Anfasa
+ */
+public class L1MenampilkanDataDiri {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Biodata" + "\n" + "-----------------" );
+        System.out.println("NAMA\t\t         : Indra Rizky Anfasa");
+        System.out.println("ALAMAT\t\t         : Talun Kab.Sumedang ");
+        System.out.println("TEMPAT LAHIR\t\t : Sumedang ");
+        System.out.println("TANGGAL LAHIR\t\t : 23 OKTOBER 2000");
+        System.out.println("--------------------------");
+        System.out.println("HOBI\t\t: Futsal");
+        System.out.println("MAKES\t\t: Martabak");
+        System.out.println("MIKES\t\t: Coffee");
+    }
+    
+}
